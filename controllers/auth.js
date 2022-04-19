5e685c34679085af747cf6c8277c48a54aeb763f
StrangeNoob <sipun63427452@gmail.com>
2022-04-19 15:15:12 +0530

    Change in signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,131 +5,170 @@ const { createJWT } = require("../utils/jwt");
 const { Users, Counter } = require("../models");
 
 const signUp = async (req, res) => {
-  const user = req.user;
+  try {
+    const user = req.user;
 
-  let newUser = await Users.findOne({ email: user.email });
-  if (newUser) {
-    return res.status(StatusCodes.BAD_REQUEST).json({
-      message: "User already exists",
-      data: newUser,
-      error: {},
-      status: StatusCodes.BAD_REQUEST,
-    });
-  }
-
-  const counter = await Counter.findOneAndUpdate(
-    {
-      _id: "entityId",
-    },
-    {
-      $inc: {
-        seq: 1,
-      },
-    },
-    {
-      new: true,
-      upsert: true,
+    let newUser = await Users.findOne({ email: user.email });
+    if (newUser) {
+      let token = createJWT(newUser);
+      return res.status(StatusCodes.OK).json({
+        message: "User already exists",
+        data: newUser,
+        error: {},
+        status: StatusCodes.OK,
+        token: token,
+      });
     }
-  );
 
-  newUser = await Users.create({
-    zid: `ZEN${String(counter.seq).padStart(6, "0")}`,
-    email: user.email,
-    profilePic: user.picture,
-  });
-  if (user.firebase.sign_in_provider === "google.com") {
-    newUser = await Users.findOneAndUpdate(
+    const counter = await Counter.findOneAndUpdate(
       {
-        email: user.email,
+        _id: "entityId",
       },
       {
-        $set: {
-          name: user.name,
+        $inc: {
+          seq: 1,
         },
       },
       {
         new: true,
+        upsert: true,
       }
     );
+
+    newUser = await Users.create({
+      zid: `ZEN${String(counter.seq).padStart(6, "0")}`,
+      email: user.email,
+      profilePic: user.picture,
+    });
+    if (user.firebase.sign_in_provider === "google.com") {
+      newUser = await Users.findOneAndUpdate(
+        {
+          email: user.email,
+        },
+        {
+          $set: {
+            name: user.name,
+          },
+        },
+        {
+          new: true,
+        }
+      );
+    }
+
+    let token = createJWT(newUser);
+
+    res.cookie("userToken", token, { maxAge: 900000, httpOnly: true });
+
+    return res.status(StatusCodes.CREATED).json({
+      message: "User created successfully",
+      data: newUser,
+      error: {},
+      token: token,
+      status: StatusCodes.CREATED,
+    });
+  } catch (err) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      message: "Internal server error",
+      data: {},
+      error: {
+        message: err.message,
+      },
+      token: null,
+      status: StatusCodes.INTERNAL_SERVER_ERROR,
+    });
   }
-
-  let token = createJWT(newUser);
-
-  res.cookie("userToken", token, { maxAge: 900000, httpOnly: true });
-
-  return res.status(StatusCodes.CREATED).json({
-    message: "User created successfully",
-    data: newUser,
-    error: {},
-    token: token,
-    status: StatusCodes.CREATED,
-  });
 };
 
 const profile = async (req, res) => {};
 
 const register = async (req, res) => {
-  const { name, phone, regNo, branch } = req.body;
-  const user = req.user;
+  try {
+    const { name, phone, regNo, branch } = req.body;
+    const user = req.user;
 
-  let newUser = await Users.findById(user._id);
-  if (newUser) {
-    newUser = await Users.findOneAndUpdate(
-      { _id: user._id },
-      {
-        $set: {
-          name,
-          phone,
-          regNo,
-          branch,
+    let newUser = await Users.findById(user._id);
+    if (newUser) {
+      newUser = await Users.findOneAndUpdate(
+        { _id: user._id },
+        {
+          $set: {
+            name,
+            phone,
+            regNo,
+            branch,
+            isVerified: true,
+          },
         },
-      },
-      {
-        new: true,
-      }
-    );
-    return res.status(StatusCodes.OK).json({
-      message: "User updated successfully",
-      data: newUser,
+        {
+          new: true,
+        }
+      );
+      return res.status(StatusCodes.OK).json({
+        message: "User updated successfully",
+        data: newUser,
+        error: {},
+        status: StatusCodes.OK,
+      });
+    }
+
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      message: "User does not exist",
+      data: {},
       error: {},
-      status: StatusCodes.OK,
+      status: StatusCodes.BAD_REQUEST,
+    });
+  } catch (err) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      message: "Internal Server Error",
+      data: {},
+      error: {
+        message: err.message,
+      },
+      token: null,
+      status: StatusCodes.INTERNAL_SERVER_ERROR,
     });
   }
-
-  return res.status(StatusCodes.BAD_REQUEST).json({
-    message: "User does not exist",
-    data: {},
-    error: {},
-    status: StatusCodes.BAD_REQUEST,
-  });
 };
 
 const signIn = async (req, res) => {
   const user = req.user;
-  let newUser = await Users.findOne({ email: user.email });
-  if (newUser) {
-    let token = createJWT(newUser);
+  try {
+    let newUser = await Users.findOne({ email: user.email });
+    if (newUser) {
+      let token = createJWT(newUser);
 
-    res.cookie("userToken", token, { maxAge: 900000, httpOnly: true });
-  
-    return res.status(StatusCodes.OK).json({
-      message: "User signed in successfully",
+      res.cookie("userToken", token, { maxAge: 900000, httpOnly: true });
+
+      return res.status(StatusCodes.OK).json({
+        message: "User signed in successfully",
+        data: newUser,
+        error: {},
+        status: StatusCodes.OK,
+        token: token,
+      });
+    }
+    return res.status(StatusCodes.NOT_FOUND).json({
+      message: "User does not exist",
+      data: {},
+      error: {
+        message: "User does not exist",
+      },
+      status: StatusCodes.NOT_FOUND,
+      token: null,
+    });
+  } catch (err) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      message: "User created successfully",
       data: newUser,
-      error: {},
-      status: StatusCodes.OK,
-      token: token,
+      error: {
+        message: err.message,
+      },
+      token: null,
+      status: StatusCodes.INTERNAL_SERVER_ERROR,
     });
   }
-  return res.status(StatusCodes.NOT_FOUND).json({
-    message: "User does not exist",
-    data: {},
-    error: {
-      message: "User does not exist",
-    },
-    status: StatusCodes.NOT_FOUND,  
-    token: null
-  });
-}
+};
 
 module.exports = {
   signUp,
--- validators/auth.js
+++ validators/auth.js
@@ -1,143 +1,159 @@
-const { StatusCodes } = require('http-status-codes');
-const firebaseAdmin = require('../utils/firebase-admin')
-const { Users } = require('../models/index');
-const { verifyJWT } = require('../utils/jwt');
+const { StatusCodes } = require("http-status-codes");
+const firebaseAdmin = require("../utils/firebase-admin");
+const { Users } = require("../models/index");
+const { verifyJWT } = require("../utils/jwt");
 
-const signUp = async (req,res,next) => {
-    const { authorization } = req.headers;
-    if(authorization == null ) {
-        return res.status(StatusCodes.UNAUTHORIZED).json({
-            message: "Authorization header is missing",
-            data: {},
-            error: {
-                message: "Authorization header is missing"
-            },
-            status: StatusCodes.UNAUTHORIZED
-        });
-    } else  {
-        try {
-            const user = await firebaseAdmin.auth().verifyIdToken(authorization);  
-            req.user = user;        
-            return next();
-        } catch (err) {
-            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-                message: "Internal server error",
-                data: {},
-                error: {
-                    message: err.message
-                },
-                status: StatusCodes.INTERNAL_SERVER_ERROR
-            });
-        }
-    } 
+const signUp = async (req, res, next) => {
+  const { authorization } = req.headers;
+  if (authorization == null) {
+    return res.status(StatusCodes.UNAUTHORIZED).json({
+      message: "Authorization header is missing",
+      data: {},
+      error: {
+        message: "Authorization header is missing",
+      },
+      status: StatusCodes.UNAUTHORIZED,
+    });
+  } else {
+    try {
+      const user = await firebaseAdmin.auth().verifyIdToken(authorization);
+      req.user = user;
+      return next();
+    } catch (err) {
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        message: "Internal server error",
+        data: {},
+        error: {
+          message: err.message,
+        },
+        status: StatusCodes.INTERNAL_SERVER_ERROR,
+      });
+    }
+  }
+};
 
-}
-
-const register = async (req,res,next) => {
+const register = async (req, res, next) => {
+  try {
     const { authorization } = req.headers;
     const { name, phone, regNo, branch } = req.body;
-    if(authorization == null ) {
-        return res.status(StatusCodes.UNAUTHORIZED).json({
-            message: "Authorization header is missing",
-            data: {},
-            error: {
-                message: "Authorization header is missing"
-            },
-            status: StatusCodes.UNAUTHORIZED
-        });
-    } else if(name == null || phone == null || regNo == null || branch == null) {
-        console.log(name, phone, regNo, branch);
-        return res.status(StatusCodes.BAD_REQUEST).json({
-            message: "Name, Phone, regNo and branch are required",
-            data: {},
-            error: {
-                message: "Name, Phone, regNo and branch are required"
-            },
-            status: StatusCodes.BAD_REQUEST
-        });
-    } else if ( !String(phone).match(/^[0-9]{10}$/) || !String(regNo).match(/^[0-9]{10}$/) ) {
-        return res.status(StatusCodes.BAD_REQUEST).json({
-            message: "Phone and regNo must be 10 digits and regNo must be 10 digits",
-            data: {},
-            error: {
-                message: "Phone and regNo must be 10 digits and regNo must be 10 digits"
-            },
-            status: StatusCodes.BAD_REQUEST
-        });
-    } else if(branch == null) {
-        return res.status(StatusCodes.BAD_REQUEST).json({
-            message: "Branch is required",
-            data: {},
-            error: {
-                message: "Branch is required"
-            },
-            status: StatusCodes.BAD_REQUEST
-        });
-    } 
-    try {
-        const { status, data, error } = verifyJWT(authorization);
-        if(status) {
-            const user = await Users.findById({ _id: data.id });
-            req.user = user;
-            return next();
-        } 
-        return res.status(StatusCodes.UNAUTHORIZED).json({
-            message: "JWT Verification Failed",
-            data: {},
-            error: {
-                message: error,
-            },
-            status: StatusCodes.UNAUTHORIZED
-        });
-    } catch (err) {
+    if (authorization == null) {
+      return res.status(StatusCodes.UNAUTHORIZED).json({
+        message: "Authorization header is missing",
+        data: {},
+        error: {
+          message: "Authorization header is missing",
+        },
+        status: StatusCodes.UNAUTHORIZED,
+      });
+    } else if (
+      name == null ||
+      phone == null ||
+      regNo == null ||
+      branch == null
+    ) {
+      console.log(name, phone, regNo, branch);
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        message: "Name, Phone, regNo and branch are required",
+        data: {},
+        error: {
+          message: "Name, Phone, regNo and branch are required",
+        },
+        status: StatusCodes.BAD_REQUEST,
+      });
+    } else if (
+      !String(phone).match(/^[0-9]{10}$/) ||
+      !String(regNo).match(/^[0-9]{10}$/)
+    ) {
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        message:
+          "Phone and regNo must be 10 digits and regNo must be 10 digits",
+        data: {},
+        error: {
+          message:
+            "Phone and regNo must be 10 digits and regNo must be 10 digits",
+        },
+        status: StatusCodes.BAD_REQUEST,
+      });
+    } else if (branch == null) {
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        message: "Branch is required",
+        data: {},
+        error: {
+          message: "Branch is required",
+        },
+        status: StatusCodes.BAD_REQUEST,
+      });
+    }
+
+    const { status, data, error } = verifyJWT(authorization);
+    if (status) {
+      const user = await Users.findById({ _id: data.id });
+      req.user = user;
+      return next();
+    }
+    return res.status(StatusCodes.UNAUTHORIZED).json({
+      message: "JWT Verification Failed",
+      data: {},
+      error: {
+        message: error,
+      },
+      status: StatusCodes.UNAUTHORIZED,
+    });
+  } catch (err) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      message: "Internal server error",
+      data: {},
+      error: {
+        message: err.message,
+      },
+    });
+  }
+};
+
+const signIn = async (req, res, next) => {
+  const { authorization } = req.headers;
+  try {
+    if (authorization == null) {
+      return res.status(StatusCodes.UNAUTHORIZED).json({
+        message: "Authorization header is missing",
+        data: {},
+        error: {
+          message: "Authorization header is missing",
+        },
+        status: StatusCodes.UNAUTHORIZED,
+      });
+    } else {
+      try {
+        const user = await firebaseAdmin.auth().verifyIdToken(authorization);
+        req.user = user;
+        return next();
+      } catch (err) {
         return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-            message: "Internal server error",
-            data: {},
-            error: {
-                message: err.message
-            }
+          message: "Internal server error",
+          data: {},
+          error: {
+            message: err.message,
+          },
+          status: StatusCodes.INTERNAL_SERVER_ERROR,
         });
+      }
     }
-}
+  } catch (err) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      message: "Internal server error",
+      data: {},
+      error: {
+        message: err.message,
+      },
+    });
+  }
+};
 
-const signIn = async (req,res,next) => {
-    const { authorization } = req.headers;
-    if( authorization == null ) {
-        return res.status(StatusCodes.UNAUTHORIZED).json({
-            message: "Authorization header is missing",
-            data: {},
-            error: {
-                message: "Authorization header is missing"
-            },
-            status: StatusCodes.UNAUTHORIZED
-        });
-    } else  {
-        try {
-            const user = await firebaseAdmin.auth().verifyIdToken(authorization);
-            req.user = user;
-            return next();
-        } catch (err) {
-            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-                message: "Internal server error",
-                data: {},
-                error: {
-                    message: err.message
-                },
-                status: StatusCodes.INTERNAL_SERVER_ERROR
-            });
-        }
-    }
-}
-
-const profile = async (req,res) => {
-
-}
-
-
+const profile = async (req, res) => {};
 
 module.exports = {
-    signUp,
-    register,
-    profile,
-    signIn,
-}
+  signUp,
+  register,
+  profile,
+  signIn,
+};
